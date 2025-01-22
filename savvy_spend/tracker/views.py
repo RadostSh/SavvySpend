ac3883b07174ae54c495ba0fb042bb8b37a44d76
Radosveta Shopova <r.shopovaa@gmail.com>
2025-01-22 17:39:37 +0200

    Add logout

--- savvy_spend/tracker/views.py
+++ savvy_spend/tracker/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import HttpResponse
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login')
 def index(request):
     """Welcome page."""
-    return HttpResponse('Welcome!')
+    return render(request, 'index.html')
