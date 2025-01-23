5f9c432b0a3c8a432081438c263c7e108c5c895c
Radosveta Shopova <r.shopovaa@gmail.com>
2025-01-23 02:05:17 +0200

    Apply some visual modifications

--- savvy_spend/tracker/urls.py
+++ savvy_spend/tracker/urls.py
@@ -6,5 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.index),
     path('login', auth_views.LoginView.as_view()),
-    path('logout', auth_views.LogoutView.as_view()),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
