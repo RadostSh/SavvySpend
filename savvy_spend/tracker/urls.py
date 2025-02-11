5c723302adaceb2990206bc93aacde598e61d0df
Radosveta Shopova <r.shopovaa@gmail.com>
2025-02-11 18:18:07 +0200

    Create register form

--- savvy_spend/tracker/urls.py
+++ savvy_spend/tracker/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.index),
     path('login', auth_views.LoginView.as_view()),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('logout', auth_views.LogoutView.as_view()),
+    path('register', views.register)
 ]
